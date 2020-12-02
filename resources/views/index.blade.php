80f958e8736b0c1315dab57231e20295f36244a4
nbhtm <70140129@qq.com>
2020-12-02 19:55:58 +0800

    first

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,47 +5,154 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>laravel api docs</title>
+    <script src="http://libs.baidu.com/jquery/2.0.0/jquery.min.js"></script>
 
+    <!-- 最新版本的 Bootstrap 核心 CSS 文件 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- 可选的 Bootstrap 主题文件（一般不用引入） -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css"
+          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js"
+            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+            crossorigin="anonymous"></script>
 </head>
 <body>
-api.version = {{$version}}
-<br>
-@foreach($routes as $k=>$route)
-    <select>
-        @foreach($route->methods as $v)
-            <option value="{{$v}}">{{$v}}</option>
-        @endforeach
-    </select>
-    <input type="text" value="{{$route->uri}}">
-    @if(isset($request[$route->uri.'@'.$route->controllerMethod]))
-        <label>启用</label>
-        <table>
-            <thead>
-            <tr>
-                <th>参数</th>
-                <th>参数规则</th>
-                <th>示例</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($request[$route->uri.'@'.$route->controllerMethod] as $key=>$value)
-                @if(!empty($value))
-                    @foreach($value as $kk=>$vv)
+
+<div class="col-lg-8">
+    <h2> api.version = {{$version}} </h2>
+    <label for="token">jwt-token</label>
+    <input type="text" id="token" value="{{ $token }}">
+    @foreach($request as $key=>$value)
+        {{--        {{ var_dump($value) }}--}}
+        {{--接口标题 --}}
+        <div style="margin-bottom: 10px">
+            @if(isset( $value['annotation']['title']))
+                <text style="font-size: 30px;font-weight:500;margin-right: 20px">{{ $value['annotation']['title'] }}</text>
+            @endif
+            <span class="label label-{{ $value['enabled'] ? 'success' : 'danger' }}">{{ $value['enabled'] ? '启用' : '未定义' }}</span>
+        </div>
+        {{--接口地址--}}
+        <div>
+            <p><kbd>{{ $key }}</kbd></p>
+        </div>
+        <form id="{{ $value['index'] }}">
+            <div>
+                <select name="method">
+                    @if(isset($value['methods']))
+                        @foreach($value['methods'] as $k=>$v)
+                            <option value="{{$v}}">{{$v}}</option>
+                        @endforeach
+                    @endif
+                </select>
+                <input type="text" name="uri" value="{{$value['uri']}}">
+                <a href="javascript:void(0);" onclick="js_method('{{$value['index']}}')" class="btn btn-default btn-xs"
+                   role="button">提交</a>
+            </div>
+            @if(!empty($value['request']))
+                <div>
+                    <table class="table table-bordered table-hover ">
+                        <thead>
                         <tr>
-                            <td>{{$kk}}</td>
-                            <td>{{ is_string($vv) ?$vv : ''}}</td>
-                            <td><input type="text"></td>
+                            <th>参数</th>
+                            <th>是否必填</th>
+                            <th>规则</th>
+                            <th>传参</th>
                         </tr>
-                    @endforeach
-                @endif
-            @endforeach
-            </tbody>
-        </table>
-    @else
-        方法未定义
-    @endif
-    <button>提交</button>
-    <br>
-@endforeach
+                        </thead>
+                        <tbody>
+                        @foreach($value['request'] as $k=>$v)
+                            <tr>
+                                <td>{{ $k }}</td>
+                                <td>{{ in_array('required',$v) ? '是':'否'}}</td>
+                                <td>{{ implode(' | ',$v) }}</td>
+                                <td><input name="{{ $k }}" type="text" data-key="{{ $k }}"></td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            @endif
+        </form>
+        <pre style="color: #ffffff;background-color: #333333;margin-top: 20px;" hidden id="pre_{{ $value['index'] }}"></pre>
+        <hr>
+    @endforeach
+</div>
+<script>
+    function js_method(id) {
+        var t = $('#' + id).serializeArray()
+        var method;
+        var indexMethod;
+        var indexUri;
+        var uri;
+        var data = {};
+        var token = $('#token').val();
+        console.log(token)
+        $.each(t, function (i, val) {
+            if (val.name == 'uri') {
+                uri = val.value
+                indexUri = i
+            } else if (val.name == "method") {
+                method = val.value
+                indexMethod = i
+            } else if (val.value != '' && val.value != ' ') {
+                data[val.name] = val.value;
+            }
+        })
+        t.splice(indexUri, 1)
+        t.splice(indexMethod, 1)
+
+        data =  JSON.stringify(data) == '{}' ? '' : JSON.stringify(data)
+
+        console.log(data)
+        $.ajax({
+            headers: {
+                "Authorization": "Bearer " + token
+            },
+            type: method,
+            url: uri,
+            dataType: 'json',
+            data: data,
+            mimeType: "multipart/form-data",
+            contentType: 'application/json;charset=UTF-8',
+            success: function (r) {
+                var pre = $('#pre_' + id);
+                pre.html(syntaxHighlight(r));
+                pre.show()
+            },
+            error: function (r) {
+                var j = r.responseJSON;
+                var pre = $('#pre_' + id);
+                pre.html(syntaxHighlight(j));
+                pre.show()
+            }
+        })
+    }
+
+    function syntaxHighlight(json) {
+        if (typeof json != 'string') {
+            json = JSON.stringify(json, undefined, 2);
+        }
+        json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+        return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+            var cls = 'number';
+            if (/^"/.test(match)) {
+                if (/:$/.test(match)) {
+                    cls = 'key';
+                } else {
+                    cls = 'string';
+                }
+            } else if (/true|false/.test(match)) {
+                cls = 'boolean';
+            } else if (/null/.test(match)) {
+                cls = 'null';
+            }
+            return '<span class="' + cls + '">' + match + '</span>';
+        });
+    }
+</script>
 </body>
 </html>
--- src/Controllers/DocsController.php
+++ src/Controllers/DocsController.php
@@ -7,8 +7,15 @@
 namespace Szkj\ApiDocs\Controllers;
 
 use Dingo\Api\Routing\Router;
+
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Doctrine\Common\Annotations\AnnotationReader;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\Rules\In;
+use Illuminate\Validation\Rules\Unique;
+use Szkj\ApiDocs\Annotation\Title;
+use Tymon\JWTAuth\JWTAuth;
 
 class DocsController extends Controller
 {
@@ -41,30 +48,97 @@ class DocsController extends Controller
 
             $controller = get_class($v->controller);
             $controllerMethod = $v->controllerMethod;
-//              $new_controller = new $controller;
+
             $ref = new \ReflectionClass($controller);
+
+            //判断该方法是否存在
             if ($ref->hasMethod($controllerMethod)) {
-//                $method = $ref->getMethod($controllerMethod);
-//                $reader = new AnnotationReader();
-//                dd($reader->getMethodAnnotation($method,Title::class));
+                $method = $ref->getMethod($controllerMethod);
+                $reader = new AnnotationReader();
+                $myAnnotation = $reader->getMethodAnnotations(
+                    $method
+                );
+                $annotation = [];
+                //TODO 获取该方法的所有注释 目前只获取 title
+                foreach ($myAnnotation as $key => $vv) {
+                    $annotation['title'] = $vv->title;
+                }
+                $request[$v->uri . '@' . $controllerMethod]['annotation'] = $annotation;
                 $parameters = $ref->getMethod($controllerMethod)->getParameters();
+                //开始遍历 parameters 获取参数
                 foreach ($parameters as $parameter) {
                     $name = $parameter->name;
                     if ($parameter_class = $parameter->getClass()) {
                         $parameter_class_name = $parameter_class->isInstance(new Request());
                         if (!$parameter_class_name) {
                             $a = $parameter->getClass()->getMethod('rules')->class;
-                            $request[$v->uri . '@' . $controllerMethod] = (new $a())->rules();
+                            $request[$v->uri . '@' . $controllerMethod]['request'] = (new $a())->rules();
                         } else {
-                            $request[$v->uri . '@' . $controllerMethod][] = [];
+                            $request[$v->uri . '@' . $controllerMethod]['request'] = [];
                         }
                     } else {
-                        $request[$v->uri . '@' . $controllerMethod] = [$name => 'required'];
+                        $request[$v->uri . '@' . $controllerMethod]['request'] = [$name => 'required'];
                     }
                 }
             }
         }
-//        dd($request);
-        return view('szkj::index', ['routes' => $routes, 'version' => $version]);
+        $request = $this->re($routes, $request);
+        try {
+            $superadmin = DB::table('users')->where('superadmin', 1)->first()->id;
+            $token = auth()->guard('api')->tokenById($superadmin);
+        }catch (\Exception $exception){
+            $token = '';
+        }
+        return view('szkj::index', ['token'=>$token, 'version' => $version, 'request' => $request]);
+    }
+
+    public function re($routes, $request)
+    {
+        foreach ($routes as $k => $route) {
+            $request[$route->uri . '@' . $route->controllerMethod]['enabled'] = isset($request[$route->uri . '@' . $route->controllerMethod]);
+            $request[$route->uri . '@' . $route->controllerMethod]['methods'] = $route->methods;
+            $request[$route->uri . '@' . $route->controllerMethod]['uri'] = $route->uri;
+        }
+        $index = 1;
+        foreach ($request as $k => $v) {
+            if (!empty($v['request'])) {
+                $request[$k]['request'] = $this->reRequest($v['request']);
+            } else {
+                $request[$k]['request'] = [];
+            }
+            $request[$k]['index'] = $index++;
+        }
+        return $request;
+    }
+
+    public function reRequest(array $array)
+    {
+        foreach ($array as $k => $v) {
+            if (!empty($v)) {
+                if (is_array($v)) {
+                    $array[$k] = $this->isObject($v);
+                }
+                if (!is_array($v)) $array[$k] = explode('|', $v);
+            }
+        }
+
+        return $array;
+//        dd($array);
+    }
+
+    //处理对象参数
+    public function isObject($array)
+    {
+        foreach ($array as $k => $v) {
+            if (is_object($v)) {
+                if ($v instanceof In){
+                    $array[$k] = $v->__toString();
+                }
+                if ($v instanceof Unique){
+                    $array[$k] = $v->__toString();
+                }
+            }
+        }
+        return $array;
     }
 }
